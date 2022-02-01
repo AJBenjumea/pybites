cc169f54d7d1583576fb3bd83da3bf63e2570ed3
AJ Benjumea <abenjumea@vmware.com>
2022-02-01 10:36:33 -0500

    Solve 8 update

--- 8/rotate.py
+++ 8/rotate.py
@@ -2,17 +2,7 @@ def rotate(string, n):
     """Rotate characters in a string.
        Expects string and n (int) for number of characters to move.
     """
-    chars = list(string)
-    if n > 0:
-      swap = list(string[:n])
-      result = chars[n:]
-      result.extend(swap)
-    else:
-      swap = list(string[n:])
-      res = chars[:n]
-      swap.extend(res)
-      return ''.join(swap)
-    return ''.join(result)
+    return string[n:] + string[:n]
 
    
 
